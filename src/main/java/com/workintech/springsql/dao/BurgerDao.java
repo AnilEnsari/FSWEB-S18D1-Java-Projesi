98131e79a88ecf574628f149d0489afcf44413c3
AnilEnsari <anilensari@gmail.com>
2023-10-23 19:52:50 +0300

    DAO and implements done

--- src/main/java/com/workintech/springsql/dao/BurgerDao.java
+++ src/main/java/com/workintech/springsql/dao/BurgerDao.java
@@ -1,6 +1,18 @@
 package com.workintech.springsql.dao;
 
+import com.workintech.springsql.entity.Burger;
+import com.workintech.springsql.entity.Enums.BreadType;
+
+import java.util.List;
+
 public interface BurgerDao {
 
-
+Burger save (Burger burger);
+List<Burger> findAll ();
+Burger findById (int id);
+List<Burger> findByPrice (double price);
+List<Burger> findByBreadType (BreadType breadType);
+List<Burger>  findByContent (String content);
+Burger update (Burger burger);
+Burger remove  (int id);
 }
---
+++ src/main/java/com/workintech/springsql/dao/BurgerDaoImpl.java
@@ -0,0 +1,80 @@
+package com.workintech.springsql.dao;
+
+import com.workintech.springsql.entity.Burger;
+import com.workintech.springsql.entity.Enums.BreadType;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class BurgerDaoImpl implements BurgerDao{
+    private EntityManager entityManager;
+
+    @Autowired
+    public BurgerDaoImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+@Transactional
+    @Override
+    public Burger save(Burger burger) {
+        entityManager.persist(burger);
+        return burger;
+    }
+
+    @Override
+    public List<Burger> findAll() {
+        TypedQuery<Burger> query = entityManager.createQuery("SELECT b FROM Burger b", Burger.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public Burger findById(int id) {
+        Burger burger = entityManager.find(Burger.class,id);
+        return burger;
+    }
+
+    @Override
+    public List<Burger> findByPrice(double price) {
+        TypedQuery<Burger> query = entityManager.createQuery("Select b FROM Burger b Where b.price > :price" +
+                "ORDER BY b.price desc", Burger.class);
+        query.setParameter("price",price);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Burger> findByBreadType(BreadType breadType) {
+        TypedQuery<Burger> query = entityManager.createQuery("SELECT b FROM Burger b WHERE b.breadType =" +
+                " :breadType ORDER BY b.name asc", Burger.class);
+        query.setParameter("breadType",breadType);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Burger> findByContent(String content) {
+        TypedQuery<Burger> query = entityManager.createQuery("SELECT b fron Burger b WHERE b.contents like " +
+                "CONCAT('%':content'%')ORDER by b.name asc", Burger.class);
+        query.setParameter("content",content);
+
+
+        return query.getResultList();
+    }
+
+    @Transactional
+    @Override
+    public Burger update(Burger burger) {
+        entityManager.merge(burger);
+        return burger;
+    }
+
+    @Transactional
+    @Override
+    public Burger remove(int id) {
+        Burger  burger = findById(id);
+        entityManager.remove(burger);
+        return burger;
+    }
+}
